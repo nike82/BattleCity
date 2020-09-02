a81f5bc827b484d43f0659a92ef293254755ad78
nike82 <nike82@tut.by>
2020-09-02 15:44:40 +0300

    Lesson #4 added ResourceManager

--- src/Renderer/ShaderProgram.h
+++ src/Renderer/ShaderProgram.h
@@ -22,6 +22,8 @@ namespace Renderer {
         ShaderProgram(ShaderProgram&& shaderProgram) noexcept;
 
     private:
+
+
         bool createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID);
         bool m_isCompiled = false;
         GLuint m_ID = 0;
